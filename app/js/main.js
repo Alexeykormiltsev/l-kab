244617c71dd390772453fc47bb5641704ff29d79
Alexeykormiltsev <cormiltsev@yandex.ru>
2023-11-03 16:51:04 +0500

    header new2

--- app/js/main.js
+++ app/js/main.js
@@ -1,18 +1,13 @@
-"use strict";
-let bgImages = [
-    '../images/bg1.jpg',
-    '../images/bg2.jpg',
-    '../images/bg3.jpg'
-  ];
-  let bgIndex = 0;
-  let bgInterval = 10000;
-  let bgElement = document.querySelector('#bg');
-  function bgChange() {
-    bgElement.style.backgroundImage = 'url(' + bgImages[bgIndex] + ')';
-    bgIndex++;
-    if (bgIndex >= bgImages.length) {
-      bgIndex = 0;
-    }
+var bgImages = ["../images/bg1.jpg", "../images/bg2.jpg", "../images/bg3.jpg"];
+var bgIndex = 0;
+var bgInterval = 10000;
+var bgElement = document.querySelector("#bg");
+function bgChange() {
+  bgElement.style.backgroundImage = "url(" + bgImages[bgIndex] + ")";
+  bgIndex++;
+  if (bgIndex >= bgImages.length) {
+    bgIndex = 0;
   }
-  bgChange();
-  window.setInterval(bgChange, bgInterval);
+}
+bgChange();
+window.setInterval(bgChange, bgInterval);
