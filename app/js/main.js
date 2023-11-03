d7ab1698e4f6a71d9559ee3c7236004a1f2cdec1
Alexeykormiltsev <cormiltsev@yandex.ru>
2023-11-03 15:44:10 +0500

    header

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,18 @@
-$(function(){
-    new AirDatepicker('#date-min');
-    
-    new AirDatepicker('#date-max');
-});
+"use strict";
+let bgImages = [
+    '../images/bg1.jpg',
+    '../images/bg2.jpg',
+    '../images/bg3.jpg'
+  ];
+  let bgIndex = 0;
+  let bgInterval = 10000;
+  let bgElement = document.querySelector('#bg');
+  function bgChange() {
+    bgElement.style.backgroundImage = 'url(' + bgImages[bgIndex] + ')';
+    bgIndex++;
+    if (bgIndex >= bgImages.length) {
+      bgIndex = 0;
+    }
+  }
+  bgChange();
+  window.setInterval(bgChange, bgInterval);
