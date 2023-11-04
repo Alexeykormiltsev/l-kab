1c2896ad11fe3bfc61fb2fe2a8dad9bdae9f9bd9
Alexeykormiltsev <cormiltsev@yandex.ru>
2023-11-04 20:30:24 +0500

    fff

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,7 @@
-let bgImages = ["./images/bg1.jpg", "./images/bg2.jpg", "./images/bg3.jpg"];
+var bgImages = ["./images/bg1.jpg", "./images/bg2.jpg", "./images/bg3.jpg"];
+let animationDuration = 2;
 let bgIndex = 0;
-let bgInterval = 10000;
+let bgInterval = 6000;
 let bgElement = document.querySelector("#bg");
 function bgChange() {
   bgElement.style.backgroundImage = "url(" + bgImages[bgIndex] + ")";
